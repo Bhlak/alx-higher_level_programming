6411a6d5a875ed9233d7c23b118646d43cc0d878
Bhlak <bhlakcodes@proton.me>
2022-11-29 15:33:40 -0800

    Negative edge case accounted for

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,10 @@ number = random.randint(-10000, 10000)
 last = number % 10
 if last > 5:
     text = "and is greater than 5"
+    print(f"Last digit of {number} is {last} {text}")
 elif last == 0:
     text = "and is 0"
+    print(f"Last digit of {number} is {last} {text}")
 elif last < 6 and last != 0:
     text = "and is less than 6 and not 0"
-print(f"Last digit of {number} is {last} {text}")
+    print(f"Last digit of {number} is -{last} {text}")
