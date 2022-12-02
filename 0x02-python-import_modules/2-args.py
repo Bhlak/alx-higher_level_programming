4f0dcb383bbdc849292b23729d224b54af67880c
Bhlak <bhlakcodes@proton.me>
2022-12-01 16:27:53 -0800

    Fixed indexing issues

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     import sys
 
-    if len(sys.argv) == 0:
+    if len(sys.argv) == 1:
         print("0 arguments.")
-    elif len(sys.argv) == 1:
+    elif len(sys.argv) == 2:
         print("1 argument:")
     else:
-        print("{} arguments:"format(len(sys.argv)))
+        print("{} arguments:"format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv) + 1):
 
         print("{} : {}".format(i, sys.argv[i]))
