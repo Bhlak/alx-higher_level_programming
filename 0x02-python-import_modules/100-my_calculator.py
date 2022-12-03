8dabd050e61e39c3aa6ab4f81d63c17d57967fcb
Bhlak <bhlakcodes@proton.me>
2022-12-02 17:23:27 -0800

    Fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-        a = int(argv[1])
-        b = int(argv[3])
-        ops = {"+": add, "-": sub, "*": mul, "/": div}
-        if argv[2] not in list(ops.keys()):
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
-        print(f"{a} {argv[2]} {b} = {ops[argv[2]](a, b)}")
+    print(f"{a} {argv[2]} {b} = {ops[argv[2]](a, b)}")
