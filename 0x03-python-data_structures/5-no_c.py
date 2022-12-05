90b3d3189f6cc6f1bbd801666b2e449f878a6cb0
Bhlak <bhlakcodes@proton.me>
2022-12-05 12:56:27 -0800

    Attempt 3

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 def no_c(my_string):
     if not my_string:
         return None
-    new_str =  [i for i in my_string if i != 'c' and i != 'C']
+    new_str = [i for i in my_string if i != 'c' or i != 'C']
     return ''.join(new_str)
