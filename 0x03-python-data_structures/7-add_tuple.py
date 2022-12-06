27ac091fcb41835eeb128fe73c43ee580c98665b
Bhlak <bhlakcodes@proton.me>
2022-12-05 20:10:00 -0800

    Hot fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -10,7 +10,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = (0, 0)
         else:
             tuple_b = (tuple_b[0], 0)
-                    
+
     first = tuple_a[0] + tuple_b[0]
     sec = tuple_a[1] + tuple_b[1]
     return (first, sec)
