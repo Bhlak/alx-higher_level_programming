d42162b1cf6c2267535354917ae1b640ce05cd2e
Bhlak <bhlakcodes@proton.me>
2022-12-23 12:37:30 -0800

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class 'Square'"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+        """
+        Initializes a new square with a private size.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
