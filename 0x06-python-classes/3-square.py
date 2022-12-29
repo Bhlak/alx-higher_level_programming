10beed6d7e3547b049d6e6ca438a345669313b26
Bhlak <bhlakcodes@proton.me>
2022-12-29 10:32:18 -0800

    Batch Commit(2-6)

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
+
 """Defines a class 'Square'"""
 
 
 class Square:
     """Defines a square"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """
+
         Initializes a new square with a private size.
 
         Args:
+
             size (int): The size of the new square.
+
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+"""Defines a class 'Square'"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0, position=(0,0)):
+        """
+
+        Initializes a new square with a private size and position.
+
+        Args:
+
+            size (int): The size of the new square.
+            position (tuple): The coordinates of the square.
+
+        """
+        self.position = position
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                not all(isinstance(num, int) for num in value) or
+                not all(i >= 0 for i in value)):
+            raise TypeError("position must br a tuple of 2 positive integers")
+        self.__position = value
+    def area(self):
+        return (self.__size ** 2)
+
+    def my_print(self):
+        length = self.size
+        if length == 0:
+            print("")
+            return
+        j = length
+        pos = self.position
+        temp = pos[1]
+        while temp > 0:
+            print("")
+            temp -= 1
+        while length > 0:
+            print(" "*pos[0], end="")
+            print("#"*j)
+            length -= 1
