ed9b2b21dd80c5a9eddd295df2a562436ac69aec
Bhlak <bhlakcodes@proton.me>
2023-01-08 14:29:04 -0800

    Test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""Defines a function add_integer
+    Parameters: a(int)
+                b(int)
+        Returns: Sum of two values
+"""
+
+
 
 def add_integer(a, b=98):
+    """Adds two integers
+        Raises: TypeError: if values are not integers or floats
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int)and not isinstance(b, float):
