527fc1b919432bc308cc7d4cd677deb5936f6884
Bhlak <bhlakcodes@proton.me>
2023-01-09 11:22:16 -0800

    Task 1 testrun

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,5 +4,14 @@
 
 class MyList(list):
     """Defines a class Mylist with a list superclass"""
+    def __init__(self):
+        """Initializes an object of MyList while initializing
+           the superclass list.
+        """
+        super().__init__()
+
     def print_sorted(self):
+        """Prints a list of the MyList object sorted 
+           in ascending order.
+        """
         print(sorted(self))
