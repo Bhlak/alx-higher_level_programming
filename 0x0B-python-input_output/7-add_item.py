91c74dac4569cc8a9e3dfe4f45d6624ed5138ed7
Bhlak <bhlakcodes@proton.me>
2023-01-10 14:56:38 -0800

    Task 7 update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,7 @@
 import sys
 
 
-if __name__ == __main__:
+if __name__ == "xs__main__":
     save = __import__("5-save_to_json_file").save_to_json_file
     load = __import__("6-load_from_json_file").load_from_json_file
 
