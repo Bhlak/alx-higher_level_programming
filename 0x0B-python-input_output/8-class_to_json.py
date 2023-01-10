070c6a0665662902a82c87bbfa2c69b6be34d5c5
Bhlak <bhlakcodes@proton.me>
2023-01-10 15:08:19 -0800

    Task 8 update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Returns dictionary description of an object"""
 
+
 def class_to_json(obj):
     """Returns dictionary desccription of simple data structure"""
     return obj.__dict__
