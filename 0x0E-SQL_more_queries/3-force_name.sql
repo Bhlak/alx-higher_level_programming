c697eccca0292bd784c20279b0bf2904ff55088a
Bhlak <bhlakcodes@proton.me>
2023-03-01 21:40:53 +0100

    Fix attempt

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- creates a table force_name
 CREATE TABLE
-IF NOT EXISTS 'force_name' (`id` INT, `name` VARCHAR(256) NOT NULL);
+IF NOT EXISTS `force_name` (`id` INT, `name` VARCHAR(256) NOT NULL);
