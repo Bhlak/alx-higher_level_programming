4f2f6ccc4286acd08202c98c684785e388b7a932
Bhlak <bhlakcodes@proton.me>
2023-03-02 16:02:05 +0100

    Fixes

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
 -- Lists all shows contained in the database
-SELECT s.`title`. g.`genre_id` 
-FROM `tv_shows` AS s LEFT JOIN
-`tv_show_genres` AS g ON s.`id` = g.`show_id`
+SELECT s.`title`, g.`genre_id` 
+FROM `tv_shows` AS s 
+LEFT JOIN `tv_show_genres` AS g 
+ON s.`id` = g.`show_id`
 ORDER BY s.`title`, g.`genre_id`;
